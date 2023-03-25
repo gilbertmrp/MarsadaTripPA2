f5fde97d9a2e2ee6f53b2835669b2435d7266843
Gilbert Marpaung <gilbertmarpaung0@gmail.com>
2023-03-25 19:57:32 +0700

    clear content in admin

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\AdminController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.index');
 });
 
 Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
